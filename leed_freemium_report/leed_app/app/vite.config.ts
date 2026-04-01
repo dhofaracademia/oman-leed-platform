8c4771b8e727bf22014b7c9e2010e46af937eb34
dhofaracademia <dhofaracademia@gmail.com>
2026-04-01 16:20:00 +0400

    Update vite.config.ts

--- leed_freemium_report/leed_app/app/vite.config.ts
+++ leed_freemium_report/leed_app/app/vite.config.ts
@@ -2,9 +2,8 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/oman-leed-platform-main/',  // ← اسم الـ repo بالضبط
   plugins: [react()],
   resolve: {
     alias: {
